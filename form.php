e1001193adb48723e4e1bb6be81b54643a882195
Gentle_wane <adebayooluwatibi2018@gmail.com>
2022-05-27 18:57:21 +0100

    zuri_php_task

---
+++ form.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+
+<form action="user_data.php" method="post">
+Name: <input type="text" name="name"><br>
+<?php echo"<br>"; ?>
+E-mail: <input type="text" name="email"><br>
+<?php echo"<br>"; ?>
+
+<label for= "DOB"> Date of Birth: <input type="date" id="birthday" name="birthday"><br>
+<?php echo"<br>"; ?>
+
+Gender: <input type="text" name="gender"><br>
+<?php echo"<br>"; ?>
+
+<label>Country: <input type="text" name="country"><br></label>
+<?php echo"<br>"; ?>
+
+
+<input type="submit">
+</form>
+
+</body>
+</html>
